0bf097c841a5cd08dcb5a32f4cd820ef1f7b53ed
Christi Harlow <cah120671@gmail.com>
2022-08-28 18:01:47 -0400

    Deploy back end

--- db/db.js
+++ db/db.js
@@ -1,8 +1,27 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("postgres://christiharlow@localhost:5432/portfolio", {
-    logging: false,
-});
+let options = {};
+let databaseURL = process.env.DATABASE_URL;
+if (!databaseURL) {
+    // this means we're on localhost!
+    (databaseURL = "postgres://christiharlow@localhost:5432/portfolio"),
+        (options = {
+            logging: false,
+        });
+} else {
+    // we're not on localhost!
+    options = {
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+    };
+}
+
+const db = new Sequelize(databaseURL, options);
 const Project = require("./Project")(db);
 
 const connectToDB = async () => {
@@ -12,7 +31,7 @@ const connectToDB = async () => {
         db.sync();
     } catch (error) {
         console.error(error);
-        console.error("Panic! DB problems... Arg!");
+        console.error("Panic!");
     }
 };
 
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const server = express();
 const cors = require("cors");
-server.use(cors());
+server.use(
+    cors({
+        credentials: true,
+        origin: [
+            "http://localhost:3000",
+            "http://localhost:3001",
+            "https://christiharlow-portfolio-back.herokuapp.com",
+        ],
+    })
+);
 
 const { db, Project } = require("./db/db.js");
 
@@ -13,10 +22,16 @@ server.get("/projects", async (req, res) => {
     res.send({ projects: await Project.findAll() });
 });
 
+// if heroku, process.env.PORT will be provided
+let port = process.env.PORT;
+if (!port) {
+    // otherwise, fallback to localhost 3001
+    port = 3001;
+}
 server.get("/project/:id", async (req, res) => {
     res.send({ project: await Project.findByPk(req.params.id) });
 });
 
-server.listen(3001, () => {
+server.listen(port, () => {
     console.log("server running");
 });
