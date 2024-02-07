8c6596bcbe7d0e449bdecb3dbb2c5d0092b39d46
Christi Harlow <cah120671@gmail.com>
2024-02-07 11:06:10 -0500

    Update db.js

--- db/db.js
+++ db/db.js
@@ -3,28 +3,31 @@ const Sequelize = require("sequelize");
 
 let db;
 if (process.env.FLY_DATABASE_URL) {
-  console.log("Connecting to Fly.io database");
-  db = new Sequelize(process.env.FLY_DATABASE_URL, { logging: false });
+    console.log("Connecting to Fly.io database");
+    db = new Sequelize(process.env.FLY_DATABASE_URL, { logging: false });
 } else {
-  console.log("Connecting to local database");
-  // If we're running locally, use the local host connection
-  db = new Sequelize("postgres://christialeciaharlow@localhost:5432/project", {
-    logging: false,
-  });
+    console.log("Connecting to local database");
+    // If we're running locally, use the local host connection
+    db = new Sequelize(
+        "postgres://christiharlow:Kiara219*@localhost:5432/project",
+        {
+            logging: false,
+        }
+    );
 }
 
 //const db = new Sequelize(databaseURL, options);
 const Project = require("./Project")(db);
 // Connect and sync to DB
 const connectToDB = async () => {
-  try {
-    await db.authenticate();
-    console.log("Connected");
-    await db.sync(); //Sync by creating the tables based off our models if they don't already exist
-  } catch (error) {
-    console.error(error);
-    console.error("Panic!");
-  }
+    try {
+        await db.authenticate();
+        console.log("Connected");
+        await db.sync(); //Sync by creating the tables based off our models if they don't already exist
+    } catch (error) {
+        console.error(error);
+        console.error("Panic!");
+    }
 };
 
 connectToDB();
